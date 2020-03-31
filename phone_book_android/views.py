01377568d3ec7b17e37d1c855921b7416ca12d7f
Zoe Wang <zoe@motherapp.com>
2020-03-31 11:23:58 +0800

    fix to suit phonebook with react native saga

--- phone_book_android/views.py
+++ phone_book_android/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt, get_token
 from django_demo_list.utils import get_int_value
 from phone_book_android.models import Phone
-import time
+import time, json
 
 
 @require_http_methods(['GET', 'POST'])
@@ -31,14 +31,17 @@ def get_phone_list(request):
         'phone_list': [],
         'total': 0,
     }
-    time.sleep(2)
+    time.sleep(1)
     try:
-        page = int(request.GET.get('page', 1))
+        page = int(request.GET.get('page', -1))
     except Exception as e:
         print(e)
-        page = 1
-    size = 10
-    records = Phone.objects.all()[size*(page-1):size*page]
+        page = -1
+    records = Phone.objects.all()
+    results['allPhone'] = len(records)
+    if page != -1:
+        size = 15
+        records = records[:size*page]
     results['phone_list'] = get_phone_detail(records)
     results['total'] = len(records)
     return JsonResponse(results)
@@ -54,14 +57,17 @@ def get_phone_by_id(request, id):
 
 
 @require_http_methods(['POST'])
+@csrf_exempt
 def add_phone(request):
     results = {
         'status': 'error',
         'message': '',
+        'image': '',
     }
     name = request.POST.get('name', None)
     phone = request.POST.get('phone', None)
     image = request.FILES.get('image', None)
+    print(name, phone, image)
     if not name or not phone:
         results['message'] = 'Lack of name or phone field.'
         return JsonResponse(results)
@@ -69,12 +75,15 @@ def add_phone(request):
         record = Phone(name=name,phone=phone,image=image)
         record.save()
         results['status'] = 'success'
+        results['id'] = record.id
+        results['image'] = record.image.url if record.image else ''
     except Exception as e:
         results['message'] = str(e)
     return JsonResponse(results)
 
 
 @require_http_methods(['POST'])
+@csrf_exempt
 def delete_phone(request):
     results = {
         'status': 'error',
@@ -89,20 +98,27 @@ def delete_phone(request):
 
 
 @require_http_methods(['POST'])
+@csrf_exempt
 def update_phone(request):
     results = {
         'status': 'error',
         'message': '',
+        'image': '',
     }
-    phone_id = get_int_value(request.POST.get('id', None), None)
-    if not phone_id:
-        results['message'] = 'Invalid phone id field.'
     name = request.POST.get('name', None)
     phone = request.POST.get('phone', None)
     image = request.FILES.get('image', None)
-    
+    print(name, phone, image)
     try:
-        record = Phone.objects.get(id=phone_id)
+        phone_id = get_int_value(request.POST.get('id', None), None)
+        print(phone_id)
+        if phone_id:
+            record = Phone.objects.get(id=phone_id)
+        else:
+            phone_name = request.POST.get('originalName', None)
+            if not phone_name:
+                results['message'] = 'Invalid phone name field.'
+            record = Phone.objects.get(name=phone_name)
     except Exception as e:
         results['message'] = str(e)
         return JsonResponse(results)
@@ -115,6 +131,7 @@ def update_phone(request):
     try:
         record.save()
         results['status'] = 'success'
+        results['image'] = record.image.url if record.image else ''
     except Exception as e:
         results['message'] = str(e)
     return JsonResponse(results)
